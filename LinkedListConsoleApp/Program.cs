6f528c77dda42d20dfe233659a291383b56dfc05
Alex Kimutai <billroberts2001@gmail.com>
2019-12-13 11:38:41 +0300

    DLL class library that processes the Linked list and deletes the duplicates

---
+++ LinkedListConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RemoveDuplicatesLinkedList;
+using static RemoveDuplicatesLinkedList.LinkedListHelpers;
+
+namespace LinkedListConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node head = null;
+            Console.WriteLine("Insert the input");
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+
+                // int data = Int32.Parse(Console.ReadLine());
+                String data = Console.ReadLine();
+
+                head = Insert(head, data);
+            }
+
+            head = removeDuplicates(head);
+            display(head);
+            Console.ReadKey();
+
+        }
+    }
+}
--- RemoveDuplicatesLinkedList/Class1.cs
+++ RemoveDuplicatesLinkedList/Class1.cs
@@ -6,7 +6,83 @@ using System.Threading.Tasks;
 
 namespace RemoveDuplicatesLinkedList
 {
-    public class Class1
+    public class LinkedListHelpers
     {
+        public class Node
+        {
+            public String data;
+            public Node next;
+            public Node(String d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+        public static Node removeDuplicates(Node head)
+        {
+            if (head != null)
+            {
+                Node start = head;
+                int countrecord = 0;
+                while (start != null)
+                {
+                    if (start.next != null && start.data == start.next.data)
+                    {
+                        countrecord = countrecord + 1;
+                        if(countrecord >3)
+                        {
+                        start.next = start.next.next;
+                        }
+                        else
+                        {
+                            start = start.next;
+                        }
+                        
+                    }
+                    else
+                    {
+                        start = start.next;
+                    }
+                }
+            }
+            return head;
+        }
+
+        public static Node Insert(Node head, String data)
+        {
+            Node p = new Node(data);
+            if (head == null)
+            {
+                head = p;
+            }
+            else if (head.next == null)
+            {
+                head.next = p;
+            }
+            else
+            {
+                Node start = head;
+                while (start.next != null)
+                {
+                    start = start.next;
+                }
+                start.next = p;
+            }
+            return head;
+        }
+
+        public static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
     }
 }
+
+
+
+  
